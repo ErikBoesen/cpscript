3e19d1c52364e12fc97f1529d0e8a3f36af79848
ErikBoesen <me@erikboesen.com>
2017-11-02 09:24:23 -0400

    Exit if not root

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 
 if [ $(whoami) != "root" ]; then
     echo "Error: this script must be run as root!"
+    exit 1
 fi
 
 echo "Removing netcat..."
@@ -13,3 +14,5 @@ for u in $users; do
     echo $u:CyberPatriot1\! | chpasswd
 done
 echo root:CyberPatriot1\! | chpasswd
+
+# https://github.com/ErikBoesen/cpscript
